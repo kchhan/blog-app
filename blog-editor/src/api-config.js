3c17f8935170c7a2b42c5539309cdda129aa76c4
Kevin Chhan <kvnchhan@gmail.com>
2020-10-28 12:50:59 -0700

    update hostname in api-config

--- blog-editor/src/api-config.js
+++ blog-editor/src/api-config.js
@@ -5,7 +5,9 @@ const hostname = window && window.location && window.location.hostname;
 if (hostname === 'kchhan-blog-editor.herokuapp.com') {
   backendHost = 'https://kchhan-blog-api.herokuapp.com';
 } else {
-  backendHost = process.env.REACT_APP_BACKEND_HOST || 'http://localhost:5000';
+  backendHost = 'http://localhost:5000';
 }
+
+console.log(backendHost)
 
 export const API_ROOT = `${backendHost}`;
